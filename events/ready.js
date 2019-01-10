0a8793f64224e7ff1a6db39b6399b54c62442e38
DeltaRade <46230290+DeltaRade@users.noreply.github.com>
2019-01-10 19:01:38 -0400

    Delete ready.js

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-const frame = require('../oversimple');
-/**
- * @param {frame.Client} client
- */
-module.exports = (client)=>{
-	console.log('ready ' + client.user.username);
-	client.user.setActivity('testing');
-
-};
