f5e4f2bbb87b286e5690bf1d68f4f2ff2af99a56
DeltaRade <46230290+DeltaRade@users.noreply.github.com>
2019-01-28 18:30:38 -0400

    Add files via upload

--- defaultcommands/help.js
+++ defaultcommands/help.js
@@ -8,9 +8,8 @@ class HelpCommand extends Command {
 		const data = [];
 		const { commands } = client;
 		const prefix = client.prefixes.join(' or ')
-			.replace('\\\\', '')
-			.replace('\\\\', '')
-			.replace(`<@!?${client.id}>`, `@${client.username}`);
+			.replace('\\', '')
+			.replace(`<@!?${client.id}>`, `@ ${client.username}`);
 		if(!args.length) {
 			data.push('```Here\'s a list of all my commands```');
 			data.push(commands.map(x=>x.name).join(', '));
