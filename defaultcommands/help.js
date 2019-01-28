417f75518effc235bc4a5d171e77e60392766201
DeltaRade <46230290+DeltaRade@users.noreply.github.com>
2019-01-28 18:28:17 -0400

    Add files via upload

--- defaultcommands/help.js
+++ defaultcommands/help.js
@@ -7,10 +7,14 @@ class HelpCommand extends Command {
 	execute(client, message, args) {
 		const data = [];
 		const { commands } = client;
+		const prefix = client.prefixes.join(' or ')
+			.replace('\\\\', '')
+			.replace('\\\\', '')
+			.replace(`<@!?${client.id}>`, `@${client.username}`);
 		if(!args.length) {
 			data.push('```Here\'s a list of all my commands```');
 			data.push(commands.map(x=>x.name).join(', '));
-			data.push(`\nYou can use \`${client.prefixes.join(' or ').replace('\\\\', '').replace('\\\\', '').replace(`<@!?${client.id}>`, `@${client.ussername}`)}help [command name]\` to get info on a specific command!`);
+			data.push(`\nYou can use \`${prefix}help [command name]\` to get info on a specific command!`);
 
 
 			return message.author.send(data, { split: true })
@@ -34,7 +38,7 @@ class HelpCommand extends Command {
 
 		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
 		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${client.prefixes.join(' or ').replace('\\\\', '')}${command.name} ${command.usage}`);
+		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
 		data.push(`**Cooldown:** ${command.cooldown || 0} second(s)`);
 
