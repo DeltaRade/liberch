9143f9e7d85eb9729435e5b6ec39f3d33c44fcad
DeltaRade <46230290+DeltaRade@users.noreply.github.com>
2019-01-28 20:29:46 -0400

    Add files via upload

--- defaultcommands/help.js
+++ defaultcommands/help.js
@@ -13,8 +13,9 @@ class HelpCommand extends Command {
 			}
 			return x;
 		});
+		console.log(prefix);
 		prefix = prefix.join(' or ');
-		prefix = prefix.replace(`<@!?${client.id}>`, 'mention');
+		prefix = prefix.replace(`<@!?${client.id}>`, `@${client.user.tag}`);
 
 		if(!args.length) {
 			data.push('```Here\'s a list of all my commands```');
