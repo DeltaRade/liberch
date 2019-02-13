bf6c015bd09a9c8425424ce72da3468ddfabcd50
DeltaRade <46230290+DeltaRade@users.noreply.github.com>
2019-02-12 21:48:12 -0400

    Add files via upload

--- defaultcommands/help.js
+++ defaultcommands/help.js
@@ -17,8 +17,8 @@ class HelpCommand extends Command {
 
 
 		if(!args.length) {
-			data.push('```Here\'s a list of all my commands```');
-			data.push(commands.map(x=>x.name).join(', '));
+			data.push('Here\'s a list of all my commands');
+			data.push(commands.map(x=>`\`${x.name}\``).join(', '));
 			data.push(`\nYou can use \`${prefix}help [command name]\` to get info on a specific command!`);
 
 
