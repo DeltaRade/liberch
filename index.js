21444024f06b4ab33e3129040bce02a04d15fd8a
DeltaRade <coolxpress26@gmail.com>
2019-07-11 12:22:25 -0400

    update exports

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const Client = require('./src/client/object');
+const Client = require('./src/client/client');
 const Command = require('./src/command/command');
 const prompt = require('./src/prompt/prompt');
 const Utils = require('./src/utils/utils');
 const FileWatch = require('./src/filewatch/filewatch');
-const SQLite3 = require('./src/sqlite/sqlite');
 const Cooldown = require('./src/cooldowns/cooldown');
-const PostgreSQL = require('./src/postgresql/postgres');
+const JSONSettingsDB = require('./src/providers/jsonProvider');
 module.exports = {
-	Client:Client,
-	Command:Command,
-	Cooldown:Cooldown,
-	TextPrompt:prompt.TextPrompt,
-	ReactionPrompt:prompt.ReactionPrompt,
-	Utils:Utils,
-	FileWatch:FileWatch,
-	PostgreSQL:PostgreSQL,
-	SQLite3:SQLite3,
+	Client,
+	Command,
+	Cooldown,
+	TextPrompt: prompt.TextPrompt,
+	ReactionPrompt: prompt.ReactionPrompt,
+	Utils,
+	FileWatch,
+	JSONSettingsDB,
 };
